629d52875a049f2f2f87654fd3007336f1e4bfb3
Aritz Madariaga <aritzmadariaga@deusto.es>
2026-05-06 20:26:18 +0200

    fix: restore missing script setup tag in BoardView.vue

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -1,5 +1,7 @@
+<script setup lang="ts">
 import { ref, onMounted, computed, onUnmounted } from 'vue'
 import { useRouter } from 'vue-router'
+
 
 const router = useRouter()
 const messages = ref<any[]>([])
